b48d8e8711805bf2e1522713e0ca1bc316237ac1
conagyurig <youngcraig23@gmail.com>
2021-09-06 11:57:13 +0100

    remove migrate

--- Website/__init__.py
+++ Website/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 import os
 from flask_login import LoginManager
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
-migrate = Migrate()
+#migrate = Migrate()
 DB_NAME = "database.db"
 
 
